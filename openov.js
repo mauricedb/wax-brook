e267a570e91f28561557b7e21e214a62eac57b3a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-08-31 08:51:56 +0200

    Use RX

--- openov.js
+++ openov.js
@@ -12,48 +12,30 @@ function timingPointName(tp) {
 }
 
 function decodeToArray(decoded, cb) {
-
     var converter = new Converter({
         delimiter: '|',
         noheader: true
     });
 
-    converter.fromString(decoded.toString(), function(err, result) {
-        //your code here 
+    converter.fromString(decoded.toString(), (err, result) => {
         result.shift()
         result.shift()
-        var fields = result.shift()
-        var rr3 = result.filter(row => row.field1 === 'HTM' && row.field3 === 3)
+        var fields = result.shift();
 
-        //  console.log(result[1].field4)
-        if (rr3.length) {
-            var keys = Object.keys(fields)
+        var keys = Object.keys(fields);
 
-            rr3.map(r => {
-                var row = {};
-                keys.forEach(k => {
-                    var value = r[k];
-                    if (value !== '\\0') {
-                        row[camelCase(fields[k])] = value;
-                    }
-
-                    var key = `${row.localServiceLevelCode}_${row.linePlanningNumber}_${row.journeyNumber}_${row.fortifyOrderNumber}_${row.lineDirection}`
-                    if (row.journeyNumber) {
-
-                        if (row.lineDirection && row.userStopOrderNumber && (row.detectedRdY || row.tripStopStatus === 'ARRIVED') && row.lineDirection === 1) {
-                            var d = data[key] = data[key] || {};
-                            d[row.userStopOrderNumber] = 
-                            `${row['\\LDataOwnerCode']} ${row.tripStopStatus} ${timingPointName(row.timingPointCode)} ${row.expectedArrivalTime} ${row.recordedArrivalTime || 'Not yet there'} ${row.detectedRdX || '?'} ${row.detectedRdY || '?'} ${row.distanceSinceDetectedUserStop}`;
-
-                            cb(row);                                            
-                            console.log(data);
-                        }
-                    }
-                })
+        result.forEach(r => {
+            var row = {};
+            keys.forEach(k => {
+                var value = r[k];
+                if (value !== '\\0') {
+                    row[camelCase(fields[k])] = value;
+                }
             })
-        }
-    });
 
+            cb(row);                                            
+        })
+    });
 }
 
 module.exports = {
@@ -66,7 +48,6 @@ module.exports = {
         // sock.subscribe('/GOVI/KV8passtimes');
 
         sock.on('message', function(msg, buffer) {
-        // if (msg.toString().indexOf('HT') !== -1) 
             zlib.gunzip(buffer,  function(err, decoded) {
                 subject.onNext(decoded);
             });
@@ -79,6 +60,19 @@ module.exports = {
                     resolve(rows);
                 })
             )
-            .flatMap(data => data);
+            .flatMap(data => data)
+            .filter(row => !!row.journeyNumber)
+            .filter(row => row.linePlanningNumber === 3) // Only Randstad Rail 3
+            .filter(row => row.lineDirection === 1) // Only a single direction
+            .filter(row => (row.detectedRdY || row.tripStopStatus === 'ARRIVED')) // Only where we know the location
+            .do(row => {
+                var key = `${row.localServiceLevelCode}_${row.linePlanningNumber}_${row.journeyNumber}_${row.fortifyOrderNumber}_${row.lineDirection}`
+                var journey = data[key] = data[key] || {};
+                journey[row.userStopOrderNumber] = 
+                `${row['\\LDataOwnerCode']} ${row.tripStopStatus} ${timingPointName(row.timingPointCode)} ${row.expectedArrivalTime} ${row.recordedArrivalTime || 'Not yet there'} ${row.detectedRdX || '?'} ${row.detectedRdY || '?'} ${row.distanceSinceDetectedUserStop}`;
+
+                console.log('\033[2J');
+                console.log(data);
+            });
     }
 };
