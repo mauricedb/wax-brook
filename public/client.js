bfb23059671ad393335481a3e8f17ebd8daaee74
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-01 20:42:45 +0200

    Drop some markers

--- public/client.js
+++ public/client.js
@@ -33,6 +33,21 @@ $(function() {
       accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw'
   }).addTo(mymap);
 
-  
+  L.marker([52.07205, 4.494591], {
+    title: 'De Leyens'
+  }).addTo(mymap);
+  L.marker([52.06261, 4.4710093], {
+    title: 'Voorweg Hoog'
+  }).addTo(mymap);
+  L.marker([52.080505, 4.323322], {
+    title: 'Centraal Station'
+  }).addTo(mymap);
+  L.marker([52.07733, 4.314396], {
+    title: 'Spui (32002880)'
+  }).addTo(mymap);
+  L.marker([52.077194, 4.313947], {
+    title: 'Spui (32002881)'
+  }).addTo(mymap);
+
   socket.on('openov', data => console.log(data));
 });
