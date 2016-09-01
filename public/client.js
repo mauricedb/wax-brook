0823713b6b3c05b1858295f191c2911c52c63920
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-01 20:32:16 +0200

    Show full screen map

--- public/client.js
+++ public/client.js
@@ -5,29 +5,34 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
-
   var socket = io();
 
-  // setInterval(() => {
-  //   socket.emit('event', 'Maurice');
-  // }, 5000);
+  var mymap = L.map('mapid')
+    .setView([52.06, 4.4], 13)
+    // .locate({
+    //   setView: true, 
+    //   maxZoom: 13
+    // })
+    .on('zoomend', e => {
+      const bounds = e.target.getBounds();
+      console.log('zoomed to ',bounds.getNorthWest(), bounds.getSouthEast())
+    })
+    .on('dragend', e => {
+      const bounds = e.target.getBounds();
+      console.log('dragged to ',bounds.getNorthWest(), bounds.getSouthEast())
+    })
+    .on('resize', e => {
+      const bounds = e.target.getBounds();
+      console.log('resize to ',bounds.getNorthWest(), bounds.getSouthEast())
+    })
+  
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw'
+  }).addTo(mymap);
 
+  
   socket.on('openov', data => console.log(data));
 });
