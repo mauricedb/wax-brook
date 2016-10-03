41f82289b397c08d90f1526d9e76787cc1da4f83
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-10-03 08:56:11 +0200

    unref/ref

--- openov.js
+++ openov.js
@@ -12,31 +12,31 @@ var data = {};
 var vehicles = {};
 
 function getTimingPoint(code) {
-  var tp = timingPoints[code];
-  if (!tp) {
-    timingPoints[code] = {};
+    var tp = timingPoints[code];
+    if (!tp) {
+        timingPoints[code] = {};
 
-    fetch(`http://v0.ovapi.nl/tpc/${code}`)
-        .then(rsp => rsp.json())
-        .then(data => {
-            timingPoints[code] = data[code].Stop;
+        fetch(`http://v0.ovapi.nl/tpc/${code}`)
+            .then(rsp => rsp.json())
+            .then(data => {
+                timingPoints[code] = data[code].Stop;
 
-            fs.writeFile('./timingPoints.json', JSON.stringify(timingPoints, null, '  '));
-    })
+                fs.writeFile('./timingPoints.json', JSON.stringify(timingPoints, null, '  '));
+            })
 
-    return {};
-  }
-  return tp;
+        return {};
+    }
+    return tp;
 
 }
 
 function timingPointName(code) {
-  var tp = getTimingPoint(code);
+    var tp = getTimingPoint(code);
 
-  if (!tp.TimingPointCode) {
-    return code;
-  }
-  return `${code} (${timingPoints[code].TimingPointName})`;
+    if (!tp.TimingPointCode) {
+        return code;
+    }
+    return `${code} (${timingPoints[code].TimingPointName})`;
 }
 
 function decodeToArray(decoded, cb) {
@@ -56,7 +56,7 @@ function decodeToArray(decoded, cb) {
             fields.field1 = fields.field1.substr(2);
         }
         var keys = Object.keys(fields);
-        
+
         result.forEach(r => {
             var row = {};
             keys.forEach(k => {
@@ -66,29 +66,51 @@ function decodeToArray(decoded, cb) {
                 }
             })
 
-            cb(row);                                            
+            cb(row);
         })
     });
 }
 
 function monitorSocket(socket) {
-    socket.on('connect', function(fd, ep) {console.log('connect, endpoint:', ep);});
-    socket.on('connect_delay', function(fd, ep) {console.log('connect_delay, endpoint:', ep);});
-    socket.on('connect_retry', function(fd, ep) {console.log('connect_retry, endpoint:', ep);});
-    socket.on('listen', function(fd, ep) {console.log('listen, endpoint:', ep);});
-    socket.on('bind_error', function(fd, ep) {console.log('bind_error, endpoint:', ep);});
-    socket.on('accept', function(fd, ep) {console.log('accept, endpoint:', ep);});
-    socket.on('accept_error', function(fd, ep) {console.log('accept_error, endpoint:', ep);});
-    socket.on('close', function(fd, ep) {console.log('close, endpoint:', ep);});
-    socket.on('close_error', function(fd, ep) {console.log('close_error, endpoint:', ep);});
-    socket.on('disconnect', function(fd, ep) {console.log('disconnect, endpoint:', ep);});
+    socket.on('connect', function (fd, ep) {
+        console.log('connect, endpoint:', ep);
+    });
+    socket.on('connect_delay', function (fd, ep) {
+        console.log('connect_delay, endpoint:', ep);
+    });
+    socket.on('connect_retry', function (fd, ep) {
+        console.log('connect_retry, endpoint:', ep);
+    });
+    socket.on('listen', function (fd, ep) {
+        console.log('listen, endpoint:', ep);
+    });
+    socket.on('bind_error', function (fd, ep) {
+        console.log('bind_error, endpoint:', ep);
+    });
+    socket.on('accept', function (fd, ep) {
+        console.log('accept, endpoint:', ep);
+    });
+    socket.on('accept_error', function (fd, ep) {
+        console.log('accept_error, endpoint:', ep);
+    });
+    socket.on('close', function (fd, ep) {
+        console.log('close, endpoint:', ep);
+    });
+    socket.on('close_error', function (fd, ep) {
+        console.log('close_error, endpoint:', ep);
+    });
+    socket.on('disconnect', function (fd, ep) {
+        console.log('disconnect, endpoint:', ep);
+    });
 
     // Handle monitor error 
-    socket.on('monitor_error', function(err) {
+    socket.on('monitor_error', function (err) {
         console.log('Error in monitoring: %s, will restart monitoring in 5 seconds', err);
-        setTimeout(function() { socket.monitor(500, 0); }, 5000);
+        setTimeout(function () {
+            socket.monitor(1000, 0);
+        }, 5000);
     });
-    
+
     // Call monitor, check for events every 500ms and get all available events. 
     console.log('Start monitoring...');
     socket.monitor(1000, 0);
@@ -100,7 +122,7 @@ var sock = zmq.socket('sub');
 monitorSocket(sock);
 sock.connect('tcp://kv78turbo.openov.nl:7817');
 
-sock.subscribe('/GOVI/KV8passtimes/SGH');           // HTM
+sock.subscribe('/GOVI/KV8passtimes/SGH'); // HTM
 // sock.subscribe('/GOVI/KV8passtimes/Haaglanden');    // Veolia
 // sock.subscribe('/GOVI/KV8passtimes/ProvZH');        // Arriva
 
@@ -113,22 +135,22 @@ module.exports = {
 
         // sock.subscribe('/GOVI/KV8passtimes/SGH');           // HTM
 
+        sock.unref();
         // Attach the socket to the main event loop. Calling this on already attached sockets is a no-op.
         sock.ref();
 
-        sock.on('message', function(msg, buffer) {
-            zlib.gunzip(buffer,  function(err, decoded) {
+        sock.on('message', function (msg, buffer) {
+            zlib.gunzip(buffer, function (err, decoded) {
                 subject.onNext(decoded);
             });
         });
 
         return subject
             .flatMap(decoded => new Promise(resolve => {
-                    var rows = [];
-                    decodeToArray(decoded, row => rows.push(row));
-                    resolve(rows);
-                })
-            )
+                var rows = [];
+                decodeToArray(decoded, row => rows.push(row));
+                resolve(rows);
+            }))
             .flatMap(data => data)
             .filter(row => !!row.journeyNumber)
             // .filter(row => row.linePlanningNumber === 3 || row.linePlanningNumber === 4) // Only Randstad Rail
@@ -151,8 +173,8 @@ module.exports = {
                     }
                 }
             })
-            
-            .do(row => {
+
+        .do(row => {
                 var journey, key = row.key;
                 if (row.tripStopStatus === 'ARRIVED' && row.journeyStopType === 'LAST') {
                     // End of the line
@@ -171,8 +193,8 @@ module.exports = {
                     delete data[key];
                 } else {
                     var journey = data[key] = data[key] || {};
-                    journey[row.userStopOrderNumber] = 
-                    `${now} ${row.dataOwnerCode} ${row.tripStopStatus} ${timingPointName(row.timingPointCode)} ${row.expectedArrivalTime} ${row.recordedArrivalTime || 'Not yet there'} ${row.latitude || '?'} ${row.longitude || '?'} ${row.distanceSinceDetectedUserStop}`;
+                    journey[row.userStopOrderNumber] =
+                        `${now} ${row.dataOwnerCode} ${row.tripStopStatus} ${timingPointName(row.timingPointCode)} ${row.expectedArrivalTime} ${row.recordedArrivalTime || 'Not yet there'} ${row.latitude || '?'} ${row.longitude || '?'} ${row.distanceSinceDetectedUserStop}`;
                 }
 
                 // console.log('\033[2J');
@@ -192,6 +214,6 @@ module.exports = {
 
             })
             .filter(row => (row.detectedRdX || row.tripStopStatus === 'ARRIVED')) // Only where we know the location
-            ;
+        ;
     }
 };
